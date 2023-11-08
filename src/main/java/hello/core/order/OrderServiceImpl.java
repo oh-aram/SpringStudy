d2101e3988f7eb40ba32654f033adbdc3f0cfc46
oh-aram <59176735+oh-aram@users.noreply.github.com>
2023-11-08 23:57:55 +0900

    [Feature] 챕터 2

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDisconutPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDisconutPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
